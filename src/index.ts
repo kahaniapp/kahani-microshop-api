9379d1a37e3183ef5cffb63a81e9635e9ba48940
Mike McGrath <mike@shopvidz.app>
2023-08-24 13:31:49 -0400

    tooling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 import apiRouter from './api';
 
@@ -9,6 +12,11 @@ dotenv.config();
 const app: Express = express();
 // Fall back to 4000 if no port is specified
 const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
